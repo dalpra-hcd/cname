0d493ccc0856ec258525cc84b42cb833fc6a41f9
Campbell Brown <campbellbrown093@gmail.com>
2025-08-21 11:36:02 +1200

    tree-sitter example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tree_sitter::{InputEdit, Language, Parser, Point};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+    parser.set_language(&tree_sitter_rust::LANGUAGE.into()).expect("Error loading Rust grammar");
+
+    let source_code = "fn test() {}";
+    let mut tree = parser.parse(source_code, None).unwrap();
+    let root_node = tree.root_node();
+
+    println!("{}", root_node.kind());
+    println!("{}", root_node.start_position().column);
+    println!("{}", root_node.end_position().column);
 }
