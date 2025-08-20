19dd0c1dbc27e9dfc04528745bc16f931063149d
Campbell Brown <campbellbrown093@gmail.com>
2025-08-21 11:50:41 +1200

    Swap to C interpreter

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use tree_sitter::{InputEdit, Language, Parser, Point};
+use tree_sitter::Parser;
 
+fn walk(node: tree_sitter::Node, source: &str) {
+    if node.kind() == "identifier" {
+        let name = node.utf8_text(source.as_bytes()).unwrap();
+        println!("Found identifier: {}", name);
+    }
 
+    let mut cursor = node.walk();
+    for child in node.children(&mut cursor) {
+        walk(child, source);
+    }
+}
 
 fn main() {
     let mut parser = Parser::new();
-    parser.set_language(&tree_sitter_rust::LANGUAGE.into()).expect("Error loading Rust grammar");
+    parser
+        .set_language(&tree_sitter_c::LANGUAGE.into())
+        .expect("Error loading C grammar");
 
-    let source_code = "fn test() {}";
-    let mut tree = parser.parse(source_code, None).unwrap();
+    let source_code = r#"
+        int my_var = 0;
+        void BadFunctionName() {}
+    "#;
+
+    let tree = parser.parse(source_code, None).unwrap();
     let root_node = tree.root_node();
 
-    println!("{}", root_node.kind());
-    println!("{}", root_node.start_position().column);
-    println!("{}", root_node.end_position().column);
+    walk(root_node, source_code);
 }
