efc5e2c32a5a39d2025e54eb71e9b08d895ccef4
Campbell Brown <campbellbrown093@gmail.com>
2025-08-21 12:14:53 +1200

    Fix pedantic warnings

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use tree_sitter::Parser;
 fn walk(node: tree_sitter::Node, source: &str) {
     if node.kind() == "identifier" {
         let name = node.utf8_text(source.as_bytes()).unwrap();
-        println!("Found identifier: {}", name);
+        println!("Found identifier: {name}");
     }
 
     let mut cursor = node.walk();
@@ -18,10 +18,10 @@ fn main() {
         .set_language(&tree_sitter_c::LANGUAGE.into())
         .expect("Error loading C grammar");
 
-    let source_code = r#"
+    let source_code = r"
         int my_var = 0;
         void BadFunctionName() {}
-    "#;
+    ";
 
     let tree = parser.parse(source_code, None).unwrap();
     let root_node = tree.root_node();
